0e908bfbbdc831674f521ae32dacc7c5ec45f3f5
Sinnisterr <willianbruno@msn.com>
2022-11-06 23:44:12 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -3,18 +3,27 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable {	
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user") // tivemos que renomear para tb_user pois User é uma palavra reservada do banco de dados h2
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	private long id;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Estratégia de autoincremento
+	private long id;  // essa chuve é numerica e vai ser autoimplementada no banco de dados pelo comando em baixo do ID
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-	
-	
+
 	public User() {
-		
+
 	}
 
 	public User(long id, String name, String email, String phone, String password) {
@@ -26,62 +35,50 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-
 	public long getId() {
 		return id;
 	}
-
 
 	public void setId(long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getPhone() {
 		return phone;
 	}
-
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-
 	public String getPassword() {
 		return password;
 	}
-
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -94,7 +91,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return id == other.id;
 	}
-	
-	
 
 }
