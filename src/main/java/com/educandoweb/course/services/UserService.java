5c1845b54c00f1edac3ec5c5d199ba1a26b8fe96
Sinnisterr <willianbruno@msn.com>
2022-11-09 23:07:25 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service                                 //@Component registra sua classe como componente do spring e vai poder ser injetado com autowired
 public class UserService { // buscar todos usuarios e operacao buscar usuario por id para fazer isso o userservice
 							// tem que ter uma depencia para userrepository.
@@ -47,10 +49,17 @@ public class UserService { // buscar todos usuarios e operacao buscar usuario po
 	}
 	
 	public User update(Long id, User obj) {
+		try { 
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
+		
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
